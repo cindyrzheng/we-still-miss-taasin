8d715e2fd01ca8c10c040b19ae7c2ab570b94195
Taasin <taasin.saquib@gmail.com>
2020-04-11 12:20:14 -0700

    init backend

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+// require('dotenv').config({ 'path': __dirname + '/.env' })
+
+const express    = require('express');
+const bodyParser = require('body-parser');
+const cors       = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/oauth', (req, res) => {
+  // console.log(req);
+  res.send(req.code);
+});
+
+app.get('/', (req, res) => {
+    res.send("we-miss-taasin-backend");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server up!");
+});
